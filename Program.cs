e78cbd3eca02273065852a53aba97d8328e6fafe
Danny <dannyconcepcion01@gmail.com>
2020-09-20 20:27:42 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_5_practica_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numero;
+
+            numero = new int[3, 3];
+            for (int fila = 0; fila < 3; fila++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.WriteLine("Digite un numero");
+                    numero[fila, col] = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+            }
+            Console.Clear();
+
+            for (int fila = 0; fila  < 3; fila++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write(" " + numero[fila, col]);
+
+                }
+                Console.WriteLine();
+
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
